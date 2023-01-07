70cb273b9e6dc2a909ab9e35215638d23ece253b
Daktic <g@eye-catcher.com>
2023-01-07 10:51:55 -0500

    added addintional methods to the envelope database class

--- server-side/db.js
+++ server-side/db.js
@@ -7,7 +7,7 @@ const username = process.env.POSTGRES_USERNAME;
 const password = process.env.POSTGRES_PASSWORD;
 
 class EnvelopeDB {
-    constructor() {
+    constructor(userId, budget=0) {
         const connectionString = {
             user: username,
             host: 'localhost',
@@ -17,7 +17,37 @@ class EnvelopeDB {
         };
 
         this.pool = new Pool(connectionString);
+        //class variables
+        this.budget = budget;
+        this.userId = userId
     }
+    //user details
+    async getUserById(id) {
+        const response = this.pool.query(sql(
+            "SELECT " +
+            "USERNAME, " +
+            "BUDGET " +
+            "FROM PERSONAL_BUDGET.ENVELOPES.USERS " +
+            "WHERE ID = :id;"
+        ))({
+            id:id
+        })
+        return response;
+    }
+    async setUserBudget(id, amount) {
+        const response = this.pool.query(sql(
+            "UPDATE PERSONAL_BUDGET.ENVELOPES.USERS " +
+            "SET BUDGET = :amount " +
+            "WHERE ID = :id " +
+            "RETURNING BUDGET;"
+        )({
+            balance: amount,
+            id:id
+        }))
+        return response.rows[0].budget;
+    }
+
+    //envelope details
     async getEnvelopeById(envelopeId) {
         const response = this.pool.query(sql(
             "SELECT " +
@@ -30,6 +60,24 @@ class EnvelopeDB {
         }))
         return response;
     }
+
+    async setEnvelopeBudget(envelopeId, budget) {
+        const userBudget = await this.getUserById(this.userId).balance
+        if (budget < userBudget) {
+            const response = await this.pool.query(sql(
+                "UPDATE PERSONAL_BUDGET.ENVELOPES.ENVELOPE " +
+                "SET BALANCE = :budget " +
+                "WHERE ID = :id RETURNING BUDGET;"
+            ))({
+                budget: budget,
+                id: envelopeId
+            })
+            return response.rows[0].budget
+        } else {
+            return "Failed to set budget."
+        }
+    }
+    //async transferEnvelopeBudget()
 
     async getEnvelopeByCategory(category) {
         const response = this.pool.query(sql(
