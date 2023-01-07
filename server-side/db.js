fb3b1a89ad87f3a5cdad5a1a3a32cd87f6704527
Daktic <g@eye-catcher.com>
2023-01-07 10:11:05 -0500

    created database connection and connection class to interact with it.

--- server-side/db.js
+++ server-side/db.js
@@ -1,24 +1,61 @@
-// dataBase where all envelopes are stored
-const envelopes = [{
-    "name": "envelope1",
-    "category" : "test"
 
-}];
+const {Pool} = require('pg');
+require('dotenv').config()
+const sql = require('yesql').pg
 
+const username = process.env.POSTGRES_USERNAME;
+const password = process.env.POSTGRES_PASSWORD;
 
-const getEnvelopeBycategory = (value)=>{
-    //filters to only matched envelopes
-    const matchedEnvelope = envelopes.filter(envelope => envelope.category === value);
-    //checks if no envelopes were found with matching categoryName
-    if (matchedEnvelope.length===0) {
-        return `Envelope with category name: "${value}" not found!`
-    } else {
-        return matchedEnvelope;
+class EnvelopeDB {
+    constructor() {
+        const connectionString = {
+            user: username,
+            host: 'localhost',
+            database: 'personal_budget',
+            password: password,
+            port: 5432
+        };
+
+        this.pool = new Pool(connectionString);
+    }
+    async getEnvelopeById(envelopeId) {
+        const response = this.pool.query(sql(
+            "SELECT " +
+            "CATEGORY," +
+            "BALANCE " +
+            "FROM PERSONAL_BUDGET.ENVELOPES.ENVELOPE " +
+            "WHERE ID = :id;"
+        )({
+            id:envelopeId
+        }))
+        return response;
     }
 
-};
+    async getEnvelopeByCategory(category) {
+        const response = this.pool.query(sql(
+            "SELECT " +
+            "CATEGORY, " +
+            "BALANCE " +
+            "FROM PERSONAL_BUDGET.ENVELOPES.ENVELOPE " +
+            "WHERE CATEGORY = :category;"
+        )({
+            category: category
+        }))
+        return response;
+    }
+    async addEnvelope(ownerId, category, balance) {
+        const response = await this.pool.query(sql(
+            "INSERT INTO PERSONAL_BUDGET.ENVELOPES.ENVELOPE" +
+            "(OWNER_ID, CATEGORY, BALANCE) " +
+            "VALUES (:ownerId, :category, :balance) RETURNING *;")({
+                ownerId: ownerId,
+                category: category,
+                balance: balance
+            }
+        ))
+        //console.log(response.rows[0].id)
+        return response.rows[0].id
+    }
+}
+testDB = new EnvelopeDB();
 
-module.exports = {
-    envelopes,
-    getEnvelopeBycategory
-};
